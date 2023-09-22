ecf8aaf9254fcb776bac04fb663ee78548773bea
AlbLaura <albornoz.laurasol@gmail.com>
2023-09-22 11:44:08 -0300

    controlando inputs

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,22 +1,51 @@
+import { useState } from "react";
 import "./Form.css";
 import InputForm from "../InputForm"; //El archivo al llamarse "index.js" no hace falta nombrarlo en la importacion
 import ListaOpciones from "../ListaOpciones";
 import Boton from "../Boton";
 
 const Form = () => {
+
+    const [nombre, setNombre] = useState("");
+    const [puesto, setPuesto] = useState("");
+    const [foto, setFoto] = useState("");
+
     //e = evento = event
     const manejarEnvio = (e) => {
         e.preventDefault();
-        console.log("Manejar envio", e);
+        console.log("Manejar envio");
+        let datosAEnviar = {
+            nombre,
+            puesto,
+            foto
+        }
+    console.log(datosAEnviar);
     };
-
     //titulo-placeholder ejemplos de Props
     return <section className="form">
         <form onSubmit={manejarEnvio}>
             <h2>Rellena el formulario para crear el colaborador.</h2>
-            <InputForm titulo="Nombre" placeholder="Ingresar nombre" required/> 
-            <InputForm titulo="Puesto" placeholder="Ingresar puesto" required/>
-            <InputForm titulo="Foto" placeholder="Ingresar enlace de foto" required/>
+            <InputForm 
+                titulo="Nombre" 
+                placeholder="Ingresar nombre" 
+                valor={nombre} 
+                setValor={setNombre} 
+                required
+            /> 
+            <InputForm 
+                titulo="Puesto" 
+                placeholder="Ingresar puesto"
+                valor={puesto} 
+                setValor={setPuesto} 
+                required
+            />
+            <InputForm 
+                titulo="Foto" 
+                placeholder="Ingresar enlace de foto" 
+                valor={foto} 
+                setValor={setFoto} 
+                required
+            />
             <ListaOpciones/>
             <Boton>
                 Crear colaborador
--- src/components/InputForm/index.js
+++ src/components/InputForm/index.js
@@ -1,10 +1,23 @@
+/* import { useState } from "react"; */
 import "./InputForm.css";
 
 const InputForm = (props) => { // props = datos externos
+    /* const [valor, setValor] = useState(""); */
     const placeholderModificado = `${props.placeholder}...`;
+
+    const manejarCambio = (e) => {
+        console.log("cambio", e.target.value);
+        props.setValor(e.target.value);
+    };
+
     return <div className="input-form">
         <label>{ props.titulo }</label>
-        <input placeholder={placeholderModificado} required={props.required}></input>
+        <input 
+            placeholder={placeholderModificado} 
+            required={props.required} 
+            value={props.valor}
+            onChange={manejarCambio}
+        /> {/*value="" | Sirve para controlar el input*/}
     </div>;
 }
 
